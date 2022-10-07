134788cf8acd7c2bee49f14cfe1d2b0d018b5202
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-07 21:51:07 +0300

    Добавьте файлы проекта.

---
+++ Task1Subtask4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string a, b;
+        // Если не задавать значения, то программа не компилируется. 
+        Console.WriteLine("Если не задавать значения, то программа не компилируется.");
+        Console.WriteLine();
+        Console.WriteLine("Введите строку №1");
+        a = Console.ReadLine();
+        Console.WriteLine("Введите строку №2");
+        
+        b = Console.ReadLine();
+        bool c = a != b;
+
+        Console.WriteLine(c);
+        Console.ReadKey();
+    }
+}
