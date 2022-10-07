69f30f3d485c7f19333715ddb0be588f89e178d6
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-07 21:59:41 +0300

    Добавлено задание 4.1.5

---
+++ Task1Subtask5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int a, b;
+        double x, y;
+
+        // Если не задавать значения, то программа компилируется, но так неинтересно.
+
+        Console.WriteLine("Введите целое число а");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите целое число b");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Введите дробное число x");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите дробное число y");
+        y = double.Parse(Console.ReadLine());
+
+        bool c = (a < b) | (x > y);
+
+        Console.WriteLine($"Логическое выражение (a < b) | (x > y) = {c}");
+        Console.ReadKey();
+    }
+}
