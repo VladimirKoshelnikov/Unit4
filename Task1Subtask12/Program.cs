0db05037740cfac53bfa3d976fe6a0fcae759223
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-07 22:13:50 +0300

    Добавлено задание 4.1.12

---
+++ Task1Subtask12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        var a = 6;
+        var b = 6;
+
+        if (a == b)
+        {
+            Console.WriteLine("Условие истинно");
+        }
+        else
+        {
+            Console.WriteLine("Условие ложно");
+        }
+        Console.ReadKey();
+    }
+}
