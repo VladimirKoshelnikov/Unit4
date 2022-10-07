d6151fb554f88e30ac767df3ef74df2158c140aa
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-07 22:24:25 +0300

    Добавлено задание 4.1.15, 4.1.17

---
+++ Task1Subtask15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var a = 6;
+        var b = 7;
+
+        var c = a != b ? a + b : b;
+
+        Console.WriteLine(c);
+        Console.ReadKey();
+    }
+}
