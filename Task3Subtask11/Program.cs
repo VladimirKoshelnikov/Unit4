5fa844ae3ade4dcf6d9bc8e71cf6a08dc3d244fe
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-11 00:13:50 +0300

    Добавлено задание 4.3.11

---
+++ Task3Subtask11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[,] array = { 
+            { 1, 2, 3 }, 
+            { 5, 6, 7 }, 
+            { 8, 9, 10 }, 
+            { 11, 12, 13 } };
+        for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+        {
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+                Console.Write(array[i, j] + " ");
+
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}            
