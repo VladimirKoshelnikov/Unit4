2aee81e5e73ee2d492c8894aa90a0e64965a14c6
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-11 00:04:42 +0300

    Добавлены задания 4.3.4 и 4.3.7

---
+++ Task3Subtask4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        Console.WriteLine("Введите ваше имя");
+        name = Console.ReadLine();
+        Console.WriteLine("Ваше имя по буквам:");
+        for (int i = 0; i < name.Length; i++)
+        {
+            Console.Write($"{name[i]} "); 
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Первая буква вашего имени {name[0]}");
+        Console.WriteLine($"Последняя буква вашего имени {name[name.Length - 1]}");
+        Console.ReadKey();
+    }
+}
